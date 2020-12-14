a747a930fdd91163309aa1a85719f5e67d577443
suhanoves <suhanoves@gmail.com>
2020-12-14 23:58:41 +0300

    add db_logger

---
+++ app/utils/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+db_logger = logging.getLogger('database')
+db_logger.setLevel(logging.DEBUG)
+
+db_handler = logging.StreamHandler()
+db_handler.setLevel(logging.DEBUG)
+
+db_formatter = logging.Formatter('{asctime} - {name} - {levelname} - {message}', style='{')
+
+db_handler.setFormatter(db_formatter)
+db_logger.addHandler(db_handler)
