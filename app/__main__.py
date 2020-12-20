524f444ad563fce82cdfe43d63b3d0808e050ea1
suhanoves <suhanoves@gmail.com>
2020-12-20 21:55:44 +0300

    Change __main__.py by adding on_startup

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,12 @@
-import app.handlers
-from app.loader import executor
+from aiogram import Dispatcher
+from aiogram.utils import executor
 
-executor.start_polling()
+import utils
+from app.loader import dp
+
+
+async def on_startup(dispatcher: Dispatcher):
+    await utils.setup_default_commands(dispatcher)
+
+
+executor.start_polling(dp, on_startup=on_startup)
