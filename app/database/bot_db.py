a2e509fd584106da23a9a9a4d0e70ea2699f4988
suhanoves <suhanoves@gmail.com>
2020-12-21 20:30:28 +0300

    Add is_allowed and is_admin getter and setter methods

--- app/database/bot_db.py
+++ app/database/bot_db.py
@@ -79,9 +79,12 @@ class Database:
 
     def execute(self, query: str, parameters: tuple = (),
                 fetchone: bool = False, fetchall: bool = False,
-                commit: bool = False):
+                commit: bool = False) -> Union[None, tuple, list[tuple]]:
         """
-        TODO add description
+        Execute given SQL query with parameters
+        Give commit=True for save changes
+        Give fetchone=True for return one result from database
+        Give fetchall=True for return multiple results as a list from database
         """
 
         self.cursor.execute(query, parameters)
@@ -110,14 +113,19 @@ class Database:
         '''
         self.execute(query=query, commit=True)
 
-    def add_new_user(self, user: Union[AiogramUser, User], phone: str = '', email: str = '',
-                     is_admin: bool = False, is_allowed: bool = False):
-
+    @staticmethod
+    def convert_user_class(user: Union[AiogramUser, User]) -> User:
+        """
+        Check if the user is aiogram.User class and convert it to bot.User class if needed
+        """
+        convert_user = user
         if isinstance(user, AiogramUser):
-            new_user = User.from_aiogram_user(user, phone, email, is_admin, is_allowed)
-        else:
-            new_user = user
+            convert_user = User.from_aiogram_user(user)
+        return convert_user
 
+    def add_new_user(self, user: Union[AiogramUser, User]):
+
+        new_user = self.convert_user_class(user)
         query = '''
                     INSERT OR IGNORE
                     INTO Users(user_id, first_name, last_name, username, phone, email, is_admin, is_allowed)
@@ -128,28 +136,81 @@ class Database:
 
         self.execute(query=query, parameters=parameters, commit=True)
 
-    # def select_all_users(self):
-    #     sql = 'SELECT * FROM Users'
-    #     return self.execute(sql=sql, fetchall=True)
-    #
-    # @staticmethod
-    # def format_kwargs(sql, parameters: dict):
-    #     formatted_sql = sql
-    #     params = [f"{key} = ?" for key in parameters]
-    #     formatted_sql += " AND ".join(params)
-    #     return formatted_sql, tuple(parameters.values())
-    #
-    # def select_user(self, **kwargs):
-    #     prefix_sql = 'SELECT * FROM Users WHERE '
-    #     sql, parameters = self.format_kwargs(prefix_sql, kwargs)
-    #     return self.execute(sql=sql, parameters=parameters, fetchone=True)
-    #
-    # def count_users(self):
-    #     return self.execute('SELECT COUNT(*) FROM Users;', fetchone=True)
-    #
-    # def update_email(self, user_id, email):
-    #     sql = 'UPDATE Users SET email=? WHERE user_id = ?'
-    #     return self.execute(sql=sql, parameters=(email, user_id), commit=True)
-    #
-    # def delete_all_users(self):
-    #     self.execute('DELETE FROM Users WHERE True', commit=True)
+    def check_is_user_admin(self, user_id: int) -> bool:
+        """
+        Determines if the user is given admin rights
+        Return False if user does not exist in database #TODO think about it
+        """
+        query = 'SELECT is_admin FROM Users WHERE user_id=?'
+        is_admin = self.execute(query=query, parameters=(user_id,), fetchone=True)
+        return is_admin is True
+
+    def set_user_admin_rights(self, user_id: int, is_admin: bool) -> None:
+        """
+        Sets admin rights for a given user
+        """
+        query = 'UPDATE Users SET is_admin=? WHERE user_id=?'
+        self.execute(query=query, parameters=(is_admin, user_id), commit=True)
+
+    def check_is_user_allowed(self, user_id: int) -> bool:
+        """
+        Determines if the user is given admin rights
+        Return False if user does not exist in database #TODO think about it
+        """
+        query = """SELECT is_allowed FROM Users WHERE user_id=?"""
+        is_allowed = self.execute(query=query, parameters=(user_id,), fetchone=True)
+        return is_allowed is True
+
+    def set_user_allowed_rights(self, user_id: int, is_allowed: bool) -> None:
+        """
+        Sets allowed rights for a given user
+        """
+        query = 'UPDATE Users SET is_allowed=? WHERE user_id=?'
+        self.execute(query=query, parameters=(is_allowed, user_id), commit=True)
+
+    @staticmethod
+    def add_params_to_sql_query(prefix_query, parameters: dict):
+        """
+        Formatting an SQL query by adding parameters according to the template:
+        [prefix_SQL_query] [parameter] AND .... AND [parameter]
+        """
+        formatted_query = prefix_query
+        params = [f"{key} = ?" for key in parameters]
+        formatted_query += " AND ".join(params)
+        return formatted_query, tuple(parameters.values())
+
+    def get_user(self, **kwargs):
+        prefix_sql = 'SELECT * FROM Users WHERE '
+        sql, parameters = self.add_params_to_sql_query(prefix_sql, kwargs)
+        return self.execute(sql=sql, parameters=parameters, fetchone=True)
+
+    def get_users(self, **kwargs):
+        prefix_sql = 'SELECT * FROM Users WHERE '
+        sql, parameters = self.add_params_to_sql_query(prefix_sql, kwargs)
+        return self.execute(sql=sql, parameters=parameters, fetchall=True)
+
+    def change_user_allowed_status(self, user: Union[AiogramUser, User], is_allowed: Union[None, bool] = None) -> None:
+        """
+        Change permission to write bot for a given user
+        If is_allowed given sets allowed rights according to is_allowed argument
+        """
+
+# def select_all_users(self):
+#     sql = 'SELECT * FROM Users'
+#     return self.execute(sql=sql, fetchall=True)
+
+#
+# def select_user(self, **kwargs):
+#     prefix_sql = 'SELECT * FROM Users WHERE '
+#     sql, parameters = self.format_kwargs(prefix_sql, kwargs)
+#     return self.execute(sql=sql, parameters=parameters, fetchone=True)
+#
+# def count_users(self):
+#     return self.execute('SELECT COUNT(*) FROM Users;', fetchone=True)
+#
+# def update_email(self, user_id, email):
+#     sql = 'UPDATE Users SET email=? WHERE user_id = ?'
+#     return self.execute(sql=sql, parameters=(email, user_id), commit=True)
+#
+# def delete_all_users(self):
+#     self.execute('DELETE FROM Users WHERE True', commit=True)
